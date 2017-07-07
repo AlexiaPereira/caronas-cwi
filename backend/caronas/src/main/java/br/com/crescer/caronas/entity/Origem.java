4b793c13e091cb6c4a7de73375896320d8780e3b
AlexiaPereira <alexia.pereira@cwi.com.br>
2017-07-06 23:38:51 -0300

    feat(origem): adicionar mapeamento de origem.

---
+++ backend/caronas/src/main/java/br/com/crescer/caronas/entity/Origem.java
@@ -0,0 +1,103 @@
+package br.com.crescer.caronas.entity;
+
+import java.io.Serializable;
+import java.lang.Long;
+import java.math.BigInteger;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author alexia.pereira
+ */
+@Entity
+@Table(name = "ORIGEM")
+public class Origem implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID_ORIGEM")
+    private Long idOrigem;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 400)
+    @Column(name = "ENDERECO")
+    private String endereco;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "LATITUDE")
+    private BigInteger latitude;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "LONGITUDE")
+    private BigInteger longitude;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "origem")
+    private List<Rotina> rotinaList;
+
+    public Origem() {
+    }
+
+    public Origem(Long idOrigem) {
+        this.idOrigem = idOrigem;
+    }
+
+    public Origem(Long idOrigem, String endereco, BigInteger latitude, BigInteger longitude) {
+        this.idOrigem = idOrigem;
+        this.endereco = endereco;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public Long getIdOrigem() {
+        return idOrigem;
+    }
+
+    public void setIdOrigem(Long idOrigem) {
+        this.idOrigem = idOrigem;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public BigInteger getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(BigInteger latitude) {
+        this.latitude = latitude;
+    }
+
+    public BigInteger getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(BigInteger longitude) {
+        this.longitude = longitude;
+    }
+
+    @XmlTransient
+    public List<Rotina> getRotinaList() {
+        return rotinaList;
+    }
+
+    public void setRotinaList(List<Rotina> rotinaList) {
+        this.rotinaList = rotinaList;
+    }
+
+}
