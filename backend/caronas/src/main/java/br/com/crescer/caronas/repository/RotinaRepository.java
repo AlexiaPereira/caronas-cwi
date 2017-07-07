461d9b6aa223a561c965def9b658116e8ba19293
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-07-07 02:59:47 -0300

    feat(backend): adicionar findByPassageiro e padronizar espaçamentos nas entidades.

--- backend/caronas/src/main/java/br/com/crescer/caronas/repository/RotinaRepository.java
+++ backend/caronas/src/main/java/br/com/crescer/caronas/repository/RotinaRepository.java
@@ -1,6 +1,7 @@
 package br.com.crescer.caronas.repository;
 
 import br.com.crescer.caronas.entity.Rotina;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 /**
@@ -8,5 +9,7 @@ import org.springframework.data.repository.CrudRepository;
  * @author chris
  */
 public interface RotinaRepository extends CrudRepository<Rotina, Long>{
+    
+    public List<Rotina> findByPassageiro(Boolean bool);
     
 }
--- backend/caronas/src/main/java/br/com/crescer/caronas/service/RotinaService.java
+++ backend/caronas/src/main/java/br/com/crescer/caronas/service/RotinaService.java
@@ -2,6 +2,7 @@ package br.com.crescer.caronas.Service;
 
 import br.com.crescer.caronas.entity.Rotina;
 import br.com.crescer.caronas.repository.RotinaRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -33,5 +34,9 @@ public class RotinaService {
 
     public Rotina loadById(Long id) {
         return rotinaRepository.findOne(id);
+    }
+    
+    public List<Rotina> findByPassageiro (Boolean bool) {
+        return rotinaRepository.findByPassageiro(bool);
     }
 }
