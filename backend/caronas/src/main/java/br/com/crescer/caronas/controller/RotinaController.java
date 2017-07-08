725880fed4798a084afaa2107df295c25d4172e5
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-07-08 18:32:56 -0300

    feat(Rotina):  obter rotinas do usuário autenticado.

--- backend/caronas/src/main/java/br/com/crescer/caronas/controller/RotinaController.java
+++ backend/caronas/src/main/java/br/com/crescer/caronas/controller/RotinaController.java
@@ -1,8 +1,13 @@
 package br.com.crescer.caronas.Controller;
 
 import br.com.crescer.caronas.Service.RotinaService;
+import br.com.crescer.caronas.Service.UsuarioService;
 import br.com.crescer.caronas.entity.Rotina;
+import br.com.crescer.caronas.entity.Usuario;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,11 +27,20 @@ public class RotinaController {
 
     @Autowired
     RotinaService rotinaService;
+    
+    @Autowired
+    UsuarioService usuarioService;
 
     @GetMapping
     public Iterable<Rotina> findAll() {
         return rotinaService.findAll();
     }
+    
+    //@GetMapping
+    //public List<Rotina> findByUsuario(@AuthenticationPrincipal User user) {
+    //    Usuario usuario = usuarioService.findByIdAutorizacao(user.getUsername());
+    //    return rotinaService.findByUsuario(usuario);
+    //}
     
     @PostMapping
     public Rotina save(@RequestBody Rotina rotina) {
@@ -44,5 +58,4 @@ public class RotinaController {
         rotinaService.remove(rotina);
     }    
     
-    //implementar get para obter rotinas de um usuário
 }
