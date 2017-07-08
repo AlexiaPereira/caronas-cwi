331dce638b5901ef270dbf96b4fa5021b02d6a5c
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-07-08 00:35:44 -0300

    feat(matrix-google-api): passar função para receber um array.

--- frontend/js/controllers/apis.controller.js
+++ frontend/js/controllers/apis.controller.js
@@ -1,21 +1,33 @@
 modulo.controller('apisController', ['$scope', function ($scope) {
 
+$scope.matriz_lado_A = [];
+$scope.matriz_lado_B = [];
+
 $scope.click = function getMatrix() {
   var origin1 = {lat: -30.0627024, lng: -51.1613968};
   var origin2 = 'Greenwich, England';
   var origin3 = {lat: -30.0627024, lng: -51.1613968};
-  var destinationA = 'Stockholm, Sweden';
-  var destinationB = {lat: 50.087, lng: 14.421};
+  $scope.matriz_lado_A.push(origin1);
+  $scope.matriz_lado_A.push(origin2);
+  $scope.matriz_lado_A.push(origin1);
+  $scope.matriz_lado_A.push(origin2);
+  var valor1 = {lat: -28.0627024, lng: -51.1613968};
+  var valor2 = {lat: 50.087, lng: 14.421};
+  var valor3 = {lat: -30.0627024, lng: -51.1613968};
+  $scope.matriz_lado_B.push(valor1);
+  $scope.matriz_lado_B.push(valor2);
+  $scope.matriz_lado_B.push(valor3);
 
   new google.maps.DistanceMatrixService().getDistanceMatrix({
-    origins: [origin1, origin2, origin3],
-    destinations: [destinationA, destinationB],
+    origins: $scope.matriz_lado_A,
+    destinations: $scope.matriz_lado_B,
     travelMode: 'DRIVING',
     unitSystem: google.maps.UnitSystem.METRIC,
     avoidHighways: false,
     avoidTolls: false
-  }, function(response, status) {
-    console.log(response);
+  }, function(response) {
+    $scope.matriz = response;
+    console.log($scope.matriz);
   })
 }
 }]);
