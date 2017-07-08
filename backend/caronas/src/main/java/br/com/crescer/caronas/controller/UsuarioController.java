c934a258304c85fdf34f1ff5907fea4e9f2c90e3
AlexiaPereira <alexia.pereira@cwi.com.br>
2017-07-08 10:40:04 -0300

    refactor(usuarioController): renomear método de email.

--- backend/caronas/src/main/java/br/com/crescer/caronas/controller/UsuarioController.java
+++ backend/caronas/src/main/java/br/com/crescer/caronas/controller/UsuarioController.java
@@ -29,7 +29,7 @@ public class UsuarioController {
     }
     
     @GetMapping(value = "/email")
-    public Usuario loadById(@RequestBody String email) {
+    public Usuario loadByEmail(@RequestBody String email) {
         return usuarioService.findByEmail(email);
     }
     
