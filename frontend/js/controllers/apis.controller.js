bfa3cb6b54353b9c9593f8676723b8e010b59998
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-07-08 02:54:05 -0300

    fix(matrix-google-api): retirar linhas desnecessárias e ajustar para receber lista de motoristas e passageiro como parâmetro.

--- frontend/js/controllers/apis.controller.js
+++ frontend/js/controllers/apis.controller.js
@@ -1,41 +1,30 @@
 modulo.controller('apisController', ['$scope', function ($scope) {
 
-$scope.matriz_lado_A = [];
-$scope.matriz_lado_B = [];
+$scope.matrizMotoristas = [];
+$scope.matrizPassageiro = [];
+$scope.distancias = [];
 
-$scope.click = function getMatrix() {
-  var origin1 = {lat: -30.0627024, lng: -51.1613968};
-  var origin2 = {lat: -25.0627024, lng: -51.1613968};
-  $scope.matriz_lado_A.push(origin1);
-  $scope.matriz_lado_A.push(origin2);
-  var valor1 = {lat: -28.0627024, lng: -53.1613968};
-  $scope.matriz_lado_B.push(valor1);
+//os objetos do arrays tem que vir no formato {lat: valor, lng: valor}, podemos ajustar no front, se necessário
+$scope.click = function(listaMotoristas, passageiro) {
+  $scope.matrizMotoristas = listaMotoristas;
+  $scope.matrizPassageiro = passageiro;
 
   new google.maps.DistanceMatrixService().getDistanceMatrix({
-    origins: $scope.matriz_lado_A,
-    destinations: $scope.matriz_lado_B,
+    origins: $scope.matrizMotoristas,
+    destinations: $scope.matrizPassageiro,
     travelMode: 'DRIVING',
     unitSystem: google.maps.UnitSystem.METRIC,
     avoidHighways: false,
     avoidTolls: false
   }, function(response) {
     $scope.matriz = response;
-    console.log($scope.matriz);
-    $scope.distancia = $scope.matriz.rows[0].elements[0].distance.value;
-    console.log($scope.distancia);
 
-    //pegar distancia de cada linha
     var i = 0;
     for (var linha in $scope.matriz.rows) {
       $scope.distancia = $scope.matriz.rows[i].elements[0].distance.value;
-      console.log($scope.distancia);
+      $scope.distancias.push($scope.distancia);
       i++;
     }
-    /*for(i = 0; i < $scope.matriz.rows.length; i++) {
-      console.log("here");
-      $scope.distancia = $scope.matriz.rows[i].elements[i].distance.value;
-      console.log($scope.distancia);
-    }*/
   })
 }
 
