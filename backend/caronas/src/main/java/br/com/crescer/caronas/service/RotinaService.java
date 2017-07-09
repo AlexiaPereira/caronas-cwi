85dde8ac1f6bccba5f4b65d1c01f4fdefeb51fc8
AlexiaPereira <alexia.pereira@cwi.com.br>
2017-07-09 02:19:40 -0300

    feat(validarDias): adicionar metodo para validar dias da semana em comum entre duas rotinas.

--- backend/caronas/src/main/java/br/com/crescer/caronas/service/RotinaService.java
+++ backend/caronas/src/main/java/br/com/crescer/caronas/service/RotinaService.java
@@ -1,12 +1,17 @@
 package br.com.crescer.caronas.Service;
 
+import br.com.crescer.caronas.entity.DiaSemana;
 import br.com.crescer.caronas.entity.Rotina;
 import br.com.crescer.caronas.entity.RotinaDiaSemana;
-import br.com.crescer.caronas.entity.Usuario;
 import br.com.crescer.caronas.repository.RotinaRepository;
+import static com.sun.org.apache.xalan.internal.lib.ExsltDatetime.date;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import static java.util.stream.Collectors.toList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 /**
  *
@@ -17,7 +22,7 @@ public class RotinaService {
 
     @Autowired
     RotinaRepository rotinaRepository;
-    
+
     @Autowired
     RotinaDiaSemanaService rotinaDiaSemanaService;
 
@@ -44,14 +49,42 @@ public class RotinaService {
     public Rotina loadById(Long id) {
         return rotinaRepository.findOne(id);
     }
-    
-    public List<Rotina> findByPassageiro (Boolean bool) {
+
+    public List<Rotina> findByPassageiro(Boolean bool) {
         return rotinaRepository.findByPassageiro(bool);
     }
-    
+
     //public List<Rotina> findByUsuario (Usuario usuario) {
     //    return rotinaRepository.findByUsuario(usuario);
     //}
-    
-    
+    public List<Rotina> buscarRotinasDeMotoristasComHorariosCompativeis(Rotina rotina) {
+        List<Rotina> rotinasDeMotoristas = this.findByPassageiro(false);
+        List<Rotina> rotinasComDiasDaSemanaCompativeis = this.buscarDiasDaSemanaCompativeis(rotina, rotinasDeMotoristas);
+
+        return null;
+    }
+
+    private List<Rotina> buscarDiasDaSemanaCompativeis(Rotina rotinaPrincipal, List<Rotina> rotinasDeMotoristas) {
+
+        List<Rotina> rotinasComDiasDaSemanaCompativeis = new ArrayList<>();
+
+        List<String> diasDaRotinaPrincipal = rotinaPrincipal.getRotinaDiaSemanaList()
+                .stream()
+                .map(RotinaDiaSemana::getDiaSemana)
+                .map(DiaSemana::getNome)
+                .collect(toList());
+
+        for (Rotina rotinaAtual : rotinasDeMotoristas) {
+            List<String> diasDaRotinaAtual = rotinaAtual.getRotinaDiaSemanaList()
+                    .stream()
+                    .map(RotinaDiaSemana::getDiaSemana)
+                    .map(DiaSemana::getNome)
+                    .collect(toList());
+
+            if (CollectionUtils.containsAny(diasDaRotinaAtual, diasDaRotinaPrincipal)) {
+                rotinasComDiasDaSemanaCompativeis.add(rotinaAtual);
+            }
+        }
+        return rotinasComDiasDaSemanaCompativeis;
+    }
 }
