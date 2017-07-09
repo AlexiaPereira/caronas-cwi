5dcec83cabb3abc29bb1d0b9a5cd29540527e3ae
chriismichel <christopher.michel@cwi.com.br>
2017-07-09 03:22:15 -0300

    Update apis.controller.js

--- frontend/js/controllers/apis.controller.js
+++ frontend/js/controllers/apis.controller.js
@@ -11,13 +11,11 @@ $scope.matrix = function() {
   $scope.listaMotorista = [{idOrigem:{latitude:-30.0624354, longitude:-51.1749197}, idUsuario:5},
                         {idOrigem:{latitude:-30.0153303, longitude:-51.1130727}, idUsuario:4},
                         {idOrigem:{latitude:-23.5505199, longitude:-46.6333094}, idUsuario:6}]
-  console.log($scope.listaMotorista);
   $scope.matrizMotoristas = [];
   $scope.matrizPassageiro = [{lat:-29.7949175,lng:-51.1465092}];
   $scope.listaMotorista.forEach(function(motorista){
     var objeto = {lat:motorista.idOrigem.latitude, lng:motorista.idOrigem.longitude};
     $scope.matrizMotoristas.push(objeto);
-    console.log($scope.matrizMotoristas);
   });
 
   new google.maps.DistanceMatrixService().getDistanceMatrix({
@@ -29,13 +27,10 @@ $scope.matrix = function() {
     avoidTolls: false
   }, function(response) {
     $scope.matriz = response;
-    console.log(response);
     var i = 0;
     for (var linha in $scope.matriz.rows) {
       var distanciaRetorno = $scope.matriz.rows[i].elements[0].distance.value;
-      console.log(distanciaRetorno);
       $scope.distancias.push({motorista:$scope.listaMotorista[0], distancia:distanciaRetorno});
-      console.log($scope.distancias);
       i++;
     }
   })
