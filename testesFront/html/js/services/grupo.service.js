bdeffd8aab60db95fb83f251d4d61d5ae4e7f447
deordines <deordines.tomazi@cwi.com.br>
2017-07-11 07:34:50 -0300

    fix(services): alterar a variável porta de 123 para 9090.

---
+++ testesFront/html/js/services/grupo.service.js
@@ -0,0 +1,48 @@
+angular.module('app').factory('GrupoService', ['$http', function($http) {
+    
+    let porta = 9090;
+    let url = `http://localhost:${porta}/api/grupos`;
+
+    function listar() {
+        return $http.get(`${url}/usuario`);
+    }
+
+    function aceitar(usuario) {
+        return $http({
+            url: `${url}/aceitar`,
+            method: 'POST',
+            data: usuario
+        });
+    }
+
+    function recusar(usuario) {
+        return $http({
+            url: `${url}/recusar`,
+            method: 'POST',
+            data: usuario
+        });
+    }
+
+    function sair(grupo) {
+        return $http({
+            url: `${url}/sair`,
+            method: 'POST',
+            data: grupo
+        });
+    }
+
+    function remover(usuarioGrupo) {
+        return $http({
+            url: `${url}/remover`,
+            method: 'POST',
+            data: usuarioGrupo
+        });
+    }
+
+    return ({
+        listar: listar,
+        aceitar: aceitar,
+        recusar: recusar,
+        sair: sair
+    });
+}]);
