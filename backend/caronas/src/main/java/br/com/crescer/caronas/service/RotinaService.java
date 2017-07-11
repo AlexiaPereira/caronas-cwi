b2c118a363357b101f24cc57314ae1b7138ef87a
AlexiaPereira <alexia.pereira@cwi.com.br>
2017-07-10 23:57:42 -0300

    refactor(rotinaService) remover espaços.

--- backend/caronas/src/main/java/br/com/crescer/caronas/service/RotinaService.java
+++ backend/caronas/src/main/java/br/com/crescer/caronas/service/RotinaService.java
@@ -1,83 +1,1 @@
-package br.com.crescer.caronas.service;
-
-import br.com.crescer.caronas.service.ValidarHorarioService;
-import br.com.crescer.caronas.entity.Grupo;
-import br.com.crescer.caronas.entity.Rotina;
-import br.com.crescer.caronas.entity.RotinaDiaSemana;
-import br.com.crescer.caronas.entity.Usuario;
-import br.com.crescer.caronas.repository.RotinaRepository;
-import java.text.ParseException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-/**
- *
- * @author chris
- */
-@Service
-public class RotinaService {
-
-    @Autowired
-    RotinaRepository rotinaRepository;
-
-    @Autowired
-    RotinaDiaSemanaService rotinaDiaSemanaService;
-
-    @Autowired
-    UsuarioService usuarioService;
-    
-    private ValidarHorarioService validarHorarioService;
-
-    @Autowired
-    GrupoService grupoService;
-
-    public Iterable<Rotina> findAll() {
-        return rotinaRepository.findAll();
-    }
-
-    public Rotina save(Rotina rotina) {
-        List<RotinaDiaSemana> diasSemana = rotina.getRotinaDiaSemanaList();
-        for (RotinaDiaSemana rotinaDiaSemana : diasSemana) {
-            rotinaDiaSemana.setRotina(rotina);
-        }
-        
-        if (rotina.getPassageiro()) {
-            return rotinaRepository.save(rotina);
-        }
-        
-        rotina = rotinaRepository.save(rotina);
-        Grupo grupoComEssaRotina = new Grupo("Grupo de " + rotina.getUsuario().getNome(), rotina);
-        grupoService.save(grupoComEssaRotina);
-        return rotina;
-    }
-
-    public Rotina update(Rotina rotina) {
-        return rotinaRepository.save(rotina);
-    }
-
-    public void remove(Rotina rotina) {
-        rotinaRepository.delete(rotina);
-    }
-
-    public Rotina loadById(Long id) {
-        return rotinaRepository.findOne(id);
-    }
-
-    public List<Rotina> findByPassageiro(Boolean bool) {
-        return rotinaRepository.findByPassageiro(bool);
-    }
-
-    public List<Rotina> findByUsuario(Usuario usuario) {
-        return rotinaRepository.findByUsuario(usuario);
-    }
-
-    public List<Rotina> matchHorarios(Rotina rotina) throws ParseException {
-        this.validarHorarioService = new ValidarHorarioService();
-        List<Rotina> rotinasDeMotoristas = this.findByPassageiro(false);
-        return validarHorarioService.buscarRotinasDeMotoristasComHorariosCompativeis(rotina, rotinasDeMotoristas);
-    }
-
-}
+package br.com.crescer.caronas.service;import br.com.crescer.caronas.entity.Grupo;import br.com.crescer.caronas.entity.Rotina;import br.com.crescer.caronas.entity.RotinaDiaSemana;import br.com.crescer.caronas.entity.Usuario;import br.com.crescer.caronas.repository.RotinaRepository;import java.text.ParseException;import java.util.List;import org.springframework.beans.factory.annotation.Autowired;import org.springframework.stereotype.Service;/** * * * * @author chris * */@Servicepublic class RotinaService {    @Autowired    RotinaRepository rotinaRepository;    @Autowired    RotinaDiaSemanaService rotinaDiaSemanaService;    @Autowired    UsuarioService usuarioService;    private ValidarHorarioService validarHorarioService;    @Autowired    GrupoService grupoService;    public Iterable<Rotina> findAll() {        return rotinaRepository.findAll();    }    public Rotina save(Rotina rotina) {        List<RotinaDiaSemana> diasSemana = rotina.getRotinaDiaSemanaList();        for (RotinaDiaSemana rotinaDiaSemana : diasSemana) {            rotinaDiaSemana.setRotina(rotina);        }        if (rotina.getPassageiro()) {            return rotinaRepository.save(rotina);        }        rotina = rotinaRepository.save(rotina);        Grupo grupoComEssaRotina = new Grupo("Grupo de " + rotina.getUsuario().getNome(), rotina);        grupoService.save(grupoComEssaRotina);        return rotina;    }    public Rotina update(Rotina rotina) {        return rotinaRepository.save(rotina);    }    public void remove(Rotina rotina) {        rotinaRepository.delete(rotina);    }    public Rotina loadById(Long id) {        return rotinaRepository.findOne(id);    }    public List<Rotina> findByPassageiro(Boolean bool) {        return rotinaRepository.findByPassageiro(bool);    }    public List<Rotina> findByUsuario(Usuario usuario) {        return rotinaRepository.findByUsuario(usuario);    }    public List<Rotina> matchHorarios(Rotina rotina) throws ParseException {        this.validarHorarioService = new ValidarHorarioService();        List<Rotina> rotinasDeMotoristas = this.findByPassageiro(false);        return validarHorarioService.buscarRotinasDeMotoristasComHorariosCompativeis(rotina, rotinasDeMotoristas);    }}
