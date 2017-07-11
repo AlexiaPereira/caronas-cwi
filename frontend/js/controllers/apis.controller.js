a05cde26d2c01e1eac510f9280a2c21202ffcf5e
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-07-10 23:37:18 -0300

    feat(matrix-api): alterar função para utilizar objetos recebidos por parâmetro.

--- frontend/js/controllers/apis.controller.js
+++ frontend/js/controllers/apis.controller.js
@@ -9,15 +9,17 @@ angular.module('app').controller('apisController', ['$scope', function ($scope) 
   var matrizPassageiro = [];
   var distancias = [];
   //alterar para passar duas listas de parametro
-  $scope.matrix = function() {
-    listaMotorista = [{idOrigem:{latitude:-30.0624354, longitude:-51.1749197}, idUsuario:5},
+  $scope.matrix = function(rotinaPassageiro, listaDeRotinasMotorista) {
+    /*listaMotorista = [{idOrigem:{latitude:-30.0624354, longitude:-51.1749197}, idUsuario:5},
       {idOrigem:{latitude:-30.0153303, longitude:-51.1130727}, idUsuario:4},
       {idOrigem:{latitude:-23.5505199, longitude:-46.6333094}, idUsuario:6}]
-      matrizPassageiro = [{lat:-29.7949175,lng:-51.1465092}];
-      listaMotorista.forEach(function(motorista){
-        var objeto = {lat:motorista.idOrigem.latitude, lng:motorista.idOrigem.longitude};
-        matrizMotoristas.push(objeto);
+      matrizPassageiro = [{lat:-29.7949175,lng:-51.1465092}];*/
+      listaDeRotinasMotorista.forEach(function(motorista){
+        let objetoMotorista = {lat:motorista.idOrigem.latitude, lng:motorista.idOrigem.longitude};
+        matrizMotoristas.push(objetoMotorista);
       });
+      let objetoPassageiro = {lat:rotinaPassageiro.idOrigem.latitude, lng:rotinaPassageiro.idOrigem.longitude};
+      matrizPassageiro.push(objetoPassageiro);
 
       new google.maps.DistanceMatrixService().getDistanceMatrix({
         origins: matrizMotoristas,
@@ -34,7 +36,6 @@ angular.module('app').controller('apisController', ['$scope', function ($scope) 
           distancias.push({motorista:listaMotorista[0], distancia:distanciaRetorno});
           i++;
         }
-        console.log(distancias);
       })
     }
 
