e26c8caf10705fda91393e0e470364f8509f508c
AlexiaPereira <alexia.pereira@cwi.com.br>
2017-07-12 08:09:32 -0300

    Revert "fix(rotinaService): arrumar rota para utilizar controller da API."
    This reverts commit b87c26ff84519ba6bf4581ec3738fb5209d2aa36.

--- backend/caronas/src/main/java/br/com/crescer/caronas/service/RotinaService.java
+++ backend/caronas/src/main/java/br/com/crescer/caronas/service/RotinaService.java
@@ -1,10 +1,13 @@
 package br.com.crescer.caronas.service;
 
+import br.com.crescer.caronas.dto.DistanciaRotina;
 import br.com.crescer.caronas.entity.Grupo;
 import br.com.crescer.caronas.entity.Rotina;
 import br.com.crescer.caronas.entity.RotinaDiaSemana;
 import br.com.crescer.caronas.entity.Usuario;
 import br.com.crescer.caronas.repository.RotinaRepository;
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.text.ParseException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -81,7 +84,6 @@ public class RotinaService {
     public List<Rotina> filtrarRotinas(Rotina rotina) throws ParseException {
         this.validarVagasService = new ValidarVagasService();
         List<Rotina> rotinasValidadasPorHorario = this.matchHorarios(rotina);
-        List<Rotina> rotinasValidasPorVagas = validarVagasService.validarVagas(rotina, rotinasValidadasPorHorario);
-        return rotinasValidasPorVagas;
+        return validarVagasService.validarVagas(rotina, rotinasValidadasPorHorario);
     }
 }
