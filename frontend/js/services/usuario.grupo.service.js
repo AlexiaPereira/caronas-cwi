12fa274a3afad6bf0df7ad2f9ac845070e8916b2
AlexiaPereira <alexia.pereira@cwi.com.br>
2017-07-14 08:09:56 -0300

    feat(sairDoGrupo): implementar método que possibilita o usuario sair de um grupo.

--- frontend/js/services/usuario.grupo.service.js
+++ frontend/js/services/usuario.grupo.service.js
@@ -3,8 +3,8 @@ angular.module('app').factory('UsuarioGrupoService', ['$http', function ($http) 
     let porta = 9090;
     let url = `http://localhost:${porta}/api/usuarios-grupo`;
 
-    function remover(idUsuarioGrupo) {
-        return $http.delete(`${url}/${idUsuarioGrupo}`);
+    function remover(idGrupo) {
+        return $http.delete(`${url}/remover/${idGrupo}`);
     }
 
     return ({
