2300c325cac72384ddc3713f7f0e6c8b2c72b744
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-07-14 05:20:56 -0300

    feat(Notificacao-backend): criar controller notificação.

---
+++ backend/caronas/src/main/java/br/com/crescer/caronas/controller/NotificacaoController.java
@@ -0,0 +1,47 @@
+package br.com.crescer.caronas.controller;
+
+import br.com.crescer.caronas.entity.Notificacao;
+import br.com.crescer.caronas.entity.Usuario;
+import br.com.crescer.caronas.service.NotificacaoService;
+import br.com.crescer.caronas.service.UsuarioService;
+import java.util.ArrayList;
+import java.util.List;
+import static java.util.stream.Collectors.toList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author chris
+ */
+@RestController
+@RequestMapping("/notificacoes")
+public class NotificacaoController {
+
+    @Autowired
+    NotificacaoService notificacaoService;
+    
+    @Autowired
+    UsuarioService usuarioService;
+
+    @GetMapping
+    public List<Notificacao> findAllByUsuario(@AuthenticationPrincipal User user) {
+        Usuario usuario = usuarioService.findByIdAutorizacao(user.getUsername());
+        return notificacaoService.findAllByUsuario(usuario);
+    }
+    
+    @DeleteMapping
+    public void removeAll(@AuthenticationPrincipal User user) {
+        Usuario usuario = usuarioService.findByIdAutorizacao(user.getUsername());
+        notificacaoService.clearByUser(usuario);
+    }
+}
