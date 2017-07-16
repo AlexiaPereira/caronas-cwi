6198e4870f114b2d5d8767d9a19f7f51e583d41a
AlexiaPereira <alexia.pereira@cwi.com.br>
2017-07-16 16:19:25 -0300

    feat(mapaFixo): adicionar propriedades necessarias para fixar o mapa no fundo.

--- frontend/js/controllers/main.controller.js
+++ frontend/js/controllers/main.controller.js
@@ -1,12 +1,18 @@
 angular.module('app').controller('MainController', ['$scope', 'authService', 'MapService', '$window', function ($scope, authService, MapService, $window) {
 
+  google.maps.event.addDomListener(window, 'load',  mapFixed());
 
   $scope.usuario = localStorage.getItem('nome');
   $scope.foto = localStorage.getItem('foto');
   $scope.logout = logout;
   var cwi = {lat: -29.7954709, lng: -51.1584814};
-  
+
   mapaCWI(cwi);
+
+  function mapFixed() {
+    var divMap = document.getElementById('map');
+    divMap.style.position = 'fixed';
+  }
 
   function logout() {
     function FabebookLogout() {
